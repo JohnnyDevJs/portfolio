6c9125d6e10b6f757340d5090aa7f02e87f26fc9
Johnny Silva <johnnydev.js@gmail.com>
2024-08-19 17:49:16 -0300

    feat: Header and footer mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,12 +9,12 @@ import {
 export function Footer() {
   return (
     <footer className="my-4 flex flex-col items-center text-center">
-      <div className="container">
+      <div className="container space-y-2 max-sm:space-y-4">
         <p className="text-center text-sm">
           © Copyright 2024 - Johnny Silva (Johnny Dev){' '}
           <code className="font-bold text-primary">&lt;JS/&gt;</code>
         </p>
-        <p className="flex items-center justify-center gap-2 text-sm">
+        <p className="flex flex-wrap items-center justify-center gap-2 text-sm leading-3">
           Feito com <FaHeart className="text-primary" /> por Johnny Silva,
           utilizando{' '}
           <Tooltip>
